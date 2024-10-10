685942ec0e373f4c277384bceded88057a6a1ccc
Grant Smith <109231551+grvnt@users.noreply.github.com>
2024-10-10 17:39:08 +0200

    route.ts update /blog live

--- app/api/blog/[...slug]/route.ts
+++ app/api/blog/[...slug]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+import { PassThrough, Readable } from 'stream';
+import type { IncomingMessage, ServerResponse } from 'http';
 
 const proxy = createProxyMiddleware({
   target: 'https://blog.fly100.co',
@@ -8,25 +10,30 @@ const proxy = createProxyMiddleware({
     '^/api/blog': '',
   },
   selfHandleResponse: true,
-  onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-    // Modify headers to allow the content to be served from fly100.co
-    res.removeHeader('x-frame-options');
-    return responseBuffer;
-  }),
-});
+  onProxyRes: responseInterceptor(
+    async (responseBuffer: Buffer, proxyRes: IncomingMessage, req: IncomingMessage, res: ServerResponse) => {
+      // Modify headers to allow the content to be served from fly100.co
+      res.removeHeader('x-frame-options');
+      return responseBuffer;
+    }
+  ),
+} as any); // Type assertion to avoid conflicts with Next.js types
 
 export async function GET(request: NextRequest) {
   return new Promise<NextResponse>((resolve, reject) => {
     // @ts-ignore (http-proxy-middleware doesn't have great types for Next.js)
     proxy(request, {
-      end: (proxyRes) => {
-        const response = new NextResponse(proxyRes.pipe(new PassThrough()), {
-          status: proxyRes.statusCode,
-          headers: new Headers(proxyRes.headers as any),
-        });
+      end: (proxyRes: IncomingMessage) => {
+        const response = new NextResponse(
+          Readable.toWeb(proxyRes.pipe(new PassThrough())) as ReadableStream,
+          {
+            status: proxyRes.statusCode,
+            headers: new Headers(proxyRes.headers as any),
+          }
+        );
         resolve(response);
       },
-      error: (err) => {
+      error: (err: Error) => {
         reject(err);
       },
     });
