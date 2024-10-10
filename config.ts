399b2a5a24e0f26518c8bf518ac126991383e46f
Grant Smith <109231551+grvnt@users.noreply.github.com>
2024-10-10 11:03:40 +0200

    pricing update

--- config.ts
+++ config.ts
@@ -28,7 +28,7 @@ const config: ConfigProps = {
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
         description: "The online community for budding XC pilots",
         // The price you want to display, the one user will be charged on Stripe.
-        price: 1199,
+        price: 1495,
         // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
         // priceAnchor: 199,
         features: [
