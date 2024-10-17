772ee77c5f9f17b1ffe1eb6c9f8fbb8c7197661b
Grant Smith <109231551+grvnt@users.noreply.github.com>
2024-10-17 13:56:32 +0200

    home form update

--- components/General/ConvertKitForm.tsx
+++ components/General/ConvertKitForm.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import styles from './ConvertKitForm.module.css';
 
 export default function ConvertKitForm() {
@@ -8,15 +8,15 @@ export default function ConvertKitForm() {
   const scriptRef = useRef<HTMLScriptElement | null>(null);
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://fly100.ck.page/6be775b702/index.js';
+    const script = document.createElement("script");
+    script.src = "https://fly100.ck.page/6be775b702/index.js";
     script.async = true;
-    script.setAttribute('data-uid', '6be775b702');
-    
+    script.setAttribute("data-uid", "6be775b702");
+
     scriptRef.current = script;
 
     script.onload = () => {
-      const existingForm = document.querySelector('form.formkit-form');
+      const existingForm = document.querySelector("form.formkit-form");
       if (existingForm && formContainerRef.current) {
         formContainerRef.current.appendChild(existingForm);
       }
@@ -28,7 +28,7 @@ export default function ConvertKitForm() {
       if (scriptRef.current && document.body.contains(scriptRef.current)) {
         document.body.removeChild(scriptRef.current);
       }
-      const form = document.querySelector('form.formkit-form');
+      const form = document.querySelector("form.formkit-form");
       if (form && form.parentNode) {
         form.parentNode.removeChild(form);
       }
@@ -38,13 +38,13 @@ export default function ConvertKitForm() {
   return (
     <div className="py-8">
       <div className="container mx-auto max-w-4xl">
-        <div 
+        <div
           className={`rounded-2xl overflow-hidden border-4 border-[#3B82F6] animate-pulse-border ${styles.formWrapper}`}
         >
           <div className="max-w-2xl mx-auto">
-            <div 
-              ref={formContainerRef} 
-              id="convertkit-form-container" 
+            <div
+              ref={formContainerRef}
+              id="convertkit-form-container"
               className="flex justify-center"
             />
           </div>
