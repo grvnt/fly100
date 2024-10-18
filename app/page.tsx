450ce88f52dcd8646e9210d966f35298ee7fbf9d
Grant Smith <109231551+grvnt@users.noreply.github.com>
2024-10-18 13:21:55 +0200

    home page metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ import { getAllPosts } from "../lib/blogApi";
 import { PostData } from "../types/post";
 import Header from "@/components/General/Header";
 import ConvertKitForm from "@/components/General/ConvertKitForm";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Fly100 | Become an XC Paragliding Jedi",
+  description:
+    "The online resource for XC paragliding pilots.",
+};
+
 export default async function Home() {
   const posts: PostData[] = await getAllPosts();
 
