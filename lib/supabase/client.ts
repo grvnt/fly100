e76b3b88f6a63973666a704232b6ecd5bb0f26d4
grvnt <109231551+grvnt@users.noreply.github.com>
2025-04-30 07:26:30 +0200

    Fix Supabase client exports

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createSupabaseClient(supabaseUrl, supabaseKey)
 
+export { createSupabaseClient as createClient }
 export default supabase
